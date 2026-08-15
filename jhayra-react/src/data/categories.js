15a66c4021b5a1b0cf250516ac7843d633d619a3
ajam1t <amit01071994@gmail.com>
2026-08-15 19:38:15 +0530

    Delete jhayra-react directory

--- jhayra-react/src/data/categories.js
+++
@@ -1,8 +0,0 @@
-export const CATEGORIES = [
-  { name: 'Personalized', slug: 'personalized', count: '120+ designs', image: '/Images/personalized.jpg' },
-  { name: 'Religious', slug: 'religious', count: 'Ganesha, Shiv Ji & more', image: '/Images/religious.jpg' },
-  { name: '7 Running Horses', slug: 'running-horses', count: 'Vastu approved', image: '/Images/horses.jpg' },
-  { name: 'Nature', slug: 'nature', count: 'Calming scenes', image: '/Images/nature.jpg' },
-  { name: 'Modern Art', slug: 'modern-art', count: 'Contemporary', image: '/Images/modern.jpg' },
-  { name: 'Canvas Prints', slug: 'canvas', count: 'Gallery quality', image: '/Images/canvas.jpg' },
-];
